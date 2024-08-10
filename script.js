a399bde43fe5c8d76cbc26eb627ebf11378965be
Murat <144884820+Mikarmk@users.noreply.github.com>
2024-08-10 20:23:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function embedVideo() {
+    const url = document.getElementById('videoUrl').value;
+    const videoId = extractVideoID(url);
+    if (videoId) {
+        const iframe = `<iframe src="https://rutube.ru/play/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
+        document.getElementById('videoContainer').innerHTML = iframe;
+    } else {
+        alert('Пожалуйста, введите корректный URL видео с Rutube.');
+    }
+}
+
+function extractVideoID(url) {
+    const regex = /(?:https?:\/\/)?(?:www\.)?(?:rutube\.ru\/(?:video\/|play\/embed\/|.*[?&]v=))([a-zA-Z0-9_-]+)/;
+    const matches = url.match(regex);
+    return matches ? matches[1] : null;
+}
